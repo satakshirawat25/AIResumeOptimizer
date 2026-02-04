538c5bbdbf00b98868260676be8250b8d6b798a0
satakshirawat25 <satakshirawat.25may@gmail.com>
2026-02-04 12:26:32 +0530

    dashboard page completed

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from './Dashboard.module.css'
+import CreditScoreIcon from '@mui/icons-material/CreditScore'
+import Skeleton from '@mui/material/Skeleton';
 
 const Dashboard = () => {
   return (
@@ -33,9 +35,35 @@ const Dashboard = () => {
 
        <div className={styles.jobDesc}>
         <textarea className={styles.textArea} placeholder='Paste your Job Description' rows={10} cols={50}/>
+
+        <div className={styles.AnalyzeBtn}>Analyze</div>
        </div>
         </div>
      
+     <div className={styles.DashboardRight}>
+      <div className={styles.DashboardRightTopCard}>
+        <div>Analyze with AI</div>
+
+        <img className={styles.profileImg} src={"https://photosly.in/wp-content/uploads/2024/04/cute-dp-for-girls_3.jpg"} />
+        <h2>ProfilePicture</h2>
+      </div>
+
+       {/* <div className={styles.DashboardRightTopCard}>
+        <div>Result</div>
+
+       <div style={{display:"flex",justifyContent:"center",alignItems:"center",gap:20}}>
+      <h1>75%</h1>
+      <CreditScoreIcon sx={{fontSize:22}}/>
+       </div>
+       
+       <div className={styles.feedback}>
+        <h3>Feedback</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias natus nesciunt earum officiis quam dolorum aliquid, veniam a cupiditate sed vel reprehenderit eum nulla ea necessitatibus. Recusandae tempore adipisci quae!</p>
+       </div>
+      </div> */}
+
+      <Skeleton variant='rectangular' sx={{borderRadius:"20px"}} width={370} height={300}/>
+     </div>
     </div>
   )
 }
