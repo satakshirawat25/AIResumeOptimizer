af4edfddadf69fc336e2d81405f4119cb67ec4c0
satakshirawat25 <satakshirawat.25may@gmail.com>
2026-02-04 12:40:51 +0530

    admin

--- frontend/src/components/Admin/Admin.jsx
+++ frontend/src/components/Admin/Admin.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
+import styles from './Admin.module.css'
+import Skeleton from '@mui/material/Skeleton'
 
 const Admin = () => {
   return (
-    <div>
-      <h1>Admin</h1>
+    <div className={styles.Admin}>
+      <div className={styles.AdminBlock}>
+
+        <Skeleton
+          variant='rectangular'
+          width={266}
+          height={200}
+          sx={{borderRadius:"20px"}}
+          />
+
+        <div className={styles.AdminCard}>
+          <h2>AdminPage</h2>
+          <p style={{color:"blue"}}>adm@gmail.com</p>
+          <h3>Score:50%</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum in voluptas ad dolores doloremque. Voluptatum eos asperiores quo, praesentium temporibus dolore laudantium,Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt eaque repudiandae fugiat quos sed distinctio incidunt consequatur placeat, dignissimos accusamus totam aliquam vero alias dolorem, eveniet quia dolore odit voluptatum. maxime omnis voluptates, doloremque velit labore ullam amet!</p>
+        </div>
+
+        <div className={styles.AdminCard}>
+          <h2>AdminPage</h2>
+          <p style={{color:"blue"}}>adm@gmail.com</p>
+          <h3>Score:50%</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum in voluptas ad dolores doloremque. Voluptatum eos asperiores quo, praesentium temporibus dolore laudantium, maxime omnis voluptates, doloremque velit labore ullam amet!</p>
+        </div>
+
+        <div className={styles.AdminCard}>
+          <h2>AdminPage</h2>
+          <p style={{color:"blue"}}>adm@gmail.com</p>
+          <h3>Score:50%</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum in voluptas ad dolores doloremque. Voluptatum eos asperiores quo, praesentium temporibus dolore laudantium, maxime omnis voluptates, doloremque velit labore ullam amet!</p>
+        </div>
+
+        <div className={styles.AdminCard}>
+          <h2>AdminPage</h2>
+          <p style={{color:"blue"}}>adm@gmail.com</p>
+          <h3>Score:50%</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum </p>
+        </div>
+
+        <div className={styles.AdminCard}>
+          <h2>AdminPage</h2>
+          <p style={{color:"blue"}}>adm@gmail.com</p>
+          <h3>Score:50%</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum in voluptas ad dolores doloremque. Voluptatum eos asperiores quo, praesentium temporibus dolore laudantium, maxime omnis voluptates, doloremque velit labore ullam amet!</p>
+        </div>
+
+        
+
+
+      </div>
+      
     </div>
   )
 }
